a452458c89460190773cb35252d0d887ec824300
gituserrzx <202964651@qq.com>
2020-01-03 17:54:47 +0800

    1-3

---
+++ components/Author.js
@@ -0,0 +1,14 @@
+import { Avatar, Divider } from 'antd'
+
+const Author = () => {
+	reutrn(
+		<div className="author-div comm-box">
+			<div>
+				<Avatar size='100' src='https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3470783845,4203906546&fm=26&gp=0.jpg' />
+				<div className="author-introduction">
+				专注于web和移动端开发
+				</div>
+			</div>
+		</div>
+	)
+} 
